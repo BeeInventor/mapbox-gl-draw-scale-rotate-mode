65069d9390524d813e226be612d4ecfe75573ecf
Steven Chen <steven11329@gmail.com>
2022-12-08 17:19:02 +0800

    fix: require no found

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import copy from 'rollup-plugin-copy';
-import pkg from './package.json';
+import image from '@rollup/plugin-image';
+
 
 export default [
   {
     input: 'src/index.js',
     plugins: [
+      image(),
       resolve(),
       commonjs(),
-      terser(),
-      copy({
-        targets: [{ src: 'src/img/**', dest: 'dist/img' }],
-      }),
     ],
     output: {
-      file: pkg.main,
-      format: 'umd',
+      file: 'dist/index.js',
+      format: 'es',
       exports: 'named',
       name: 'ScaleRotateMode',
       sourcemap: process.env.NODE_ENV !== 'production',
